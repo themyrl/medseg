58c87a3470c980267032995d1ce3596cbf2e40c8
themyrl <loic.themyr@lecnam.net>
2022-11-17 02:47:19 +0100

    convert data

--- CustomDataset.py
+++ CustomDataset.py
@@ -84,6 +84,8 @@ class CustomDataset(Dataset):
 				data_i = tmp[0] if self.transform is not None else data_i
 			else:
 				data_i = tmp if self.transform is not None else data_i
+
+			data_i["image"] = torch.from_numpy(data_i["image"].numpy())
 		
 
 
@@ -91,7 +93,7 @@ class CustomDataset(Dataset):
 		data_i["center"] = np.array(centers)
 
 
-		data_i["image"] = torch.from_numpy(data_i["image"].numpy())
+		
 		# Do deep supervision on labels
 		if self.net_num_pool_op_kernel_sizes!=[]:
 			deep_supervision_scales = [[1, 1, 1]] + list(list(i) for i in 1 / np.cumprod(
