c8d2fc0915eb16af1c5c9f09f8588ff6cba56ce6
themyrl <loic.themyr@lecnam.net>
2022-11-30 17:26:28 +0100

    dataset

--- CustomDataset.py
+++ CustomDataset.py
@@ -51,7 +51,8 @@ class CustomDataset(Dataset):
 
 	def __getitem__(self, index):
 		log=self.log
-		log.debug("data", self.data)
+		log.debug("index", index)
+		log.debug("n_data", self.n_data)
 		if self.type == 'train':
 			if self.iterations == 0:
 				self.idx += 1
