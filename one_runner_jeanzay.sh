38c1f617cf59ebe51291977e24d463f4b9f982a7
themyrl <loic.themyr@lecnam.net>
2022-11-30 16:58:01 +0100

    dataset

--- one_runner_jeanzay.sh
+++ one_runner_jeanzay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=m_usco     # job name
+#SBATCH --job-name=m_ctnn     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/m_usco%j.out # output file name # add %j to id the job
-#SBATCH --error=logs/m_usco%j.err  # error file name # add %j to id the job
+#SBATCH --output=logs/m_ctnn%j.out # output file name # add %j to id the job
+#SBATCH --error=logs/m_ctnn%j.err  # error file name # add %j to id the job
 # # #   SBATCH -C v100-32g
 
 set -x
@@ -38,7 +38,7 @@ module load python/3.8.8
 
 ## multi anno
 # python mainDouble.py -m model=nnunet dataset=us_128_double_jz training=training_128_jz dataset.cv=$1 #m_usnn
-python mainDouble.py -m model=cotr dataset=us_128_double_jz training=training_128_jz dataset.cv=$1 #m_usco
+# python mainDouble.py -m model=cotr dataset=us_128_double_jz training=training_128_jz dataset.cv=$1 #m_usco
 
-# python mainDouble.py -m model=nnunet dataset=ct_128_final_jz training=training_128_jz dataset.cv=$1 #m_ctnn
+python mainDouble.py -m model=nnunet dataset=ct_128_final_jz training=training_128_jz dataset.cv=$1 #m_ctnn
 # python mainDouble.py -m model=cotr dataset=ct_128_final_jz training=training_128_jz dataset.cv=$1 #m_ctco
