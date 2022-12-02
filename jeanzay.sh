af27ac98ab873dd8bf1c529d24ac587db01c7488
themyrl <loic.themyr@lecnam.net>
2022-12-02 19:32:35 +0100

    nnunet full size

--- jeanzay.sh
+++ jeanzay.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=debug     # job name
+#SBATCH --job-name=fuctnn     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=00:10:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-dev
-#SBATCH --output=logs/debug.out # output file name # add %j to id the job
-#SBATCH --error=logs/debug.err  # error file name # add %j to id the job
+#SBATCH --time=99:00:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --output=logs/fuctnn.out # output file name # add %j to id the job
+#SBATCH --error=logs/fuctnn.err  # error file name # add %j to id the job
 #   SBATCH -C v100-32g
 
 set -x
@@ -59,7 +59,7 @@ module load python/3.8.8
 # python mainDouble.py -m model=nnunet dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1
 # python mainDouble.py -m model=unetr dataset=ct_128_double_jz training=training_128_jz training.loss=Dice dataset.cv=cv1 #unetrdbg
 # python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 #glamdbg
-python mainDouble.py -m model=nnunet dataset=ct_full_double_jz training=training_full_jz dataset.cv=cv1
+python mainDouble.py -m model=nnunet dataset=ct_full_double_jz training=training_full_jz dataset.cv=cv1 #fuctnn
 
 
 # Evaluation
