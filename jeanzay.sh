9267cc191e85fca5f0a64b28c530e380e894c1fe
themyrl <loic.themyr@lecnam.net>
2022-12-05 11:20:13 +0100

    dbg eval

--- jeanzay.sh
+++ jeanzay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=evglmdbg     # job name
+#SBATCH --job-name=evaldbg     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=01:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-dev
-#SBATCH --output=logs/evglmdbg.out # output file name # add %j to id the job
-#SBATCH --error=logs/evglmdbg.err  # error file name # add %j to id the job
+#SBATCH --output=logs/evaldbg.out # output file name # add %j to id the job
+#SBATCH --error=logs/evaldbg.err  # error file name # add %j to id the job
 #   SBATCH -C v100-32g
 
 set -x
@@ -59,7 +59,8 @@ module load python/3.8.8
 # python mainDouble.py -m model=nnunet dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1
 # python mainDouble.py -m model=unetr dataset=ct_128_double_jz training=training_128_jz training.loss=Dice dataset.cv=cv1 #unetrdbg
 # python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 #glamdbg
-python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 training.only_val=True #evglmdbg
+# python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 training.only_val=True #evglmdbg
+python mainDouble.py -m model=unetr dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 training.only_val=True #evglmdbg
 # python mainDouble.py -m model=nnunet dataset=ct_full_double_jz training=training_full_jz dataset.cv=cv1 #fuctnn
 
 
