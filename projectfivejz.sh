1b0bb2e90d3dae581d2c9c97d9f2479548392308
themyrl <loic.themyr@lecnam.net>
2022-12-09 16:14:08 +0100

    word

--- projectfivejz.sh
+++ projectfivejz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=glamdbgv2     # job name
+#SBATCH --job-fivelog/dataset     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=05:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/glamdbgv2.out # output file name # add %j to id the job
-#SBATCH --error=logs/glamdbgv2.err  # error file name # add %j to id the job
+#SBATCH --output=fivelog/dataset.out # output file name # add %j to id the job
+#SBATCH --error=fivelog/dataset.err  # error file name # add %j to id the job
 #   SBATCH -C v100-32g
 
 set -x
@@ -23,7 +23,7 @@ module load python/3.8.8
 
 
 
-# Dataset preprocessing
+# Dataset preprocessing #dataset
 srun python convert_dataset.py /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/imagesTr /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/imagesTr_npz
 srun python convert_dataset.py /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/labelsTr /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/labelsTr_npz
 srun python convert_dataset.py /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/imagesVal /gpfsscratch/rech/arf/unm89rb/WORD-V0.1.0/imagesVal_npz
