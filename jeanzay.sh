bcb365edfb6df4620bc5397e54046093dc7b14bd
themyrl <loic.themyr@lecnam.net>
2022-12-09 13:46:37 +0100

    dbg eval

--- jeanzay.sh
+++ jeanzay.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=glamdbg     # job name
+#SBATCH --job-name=evglmdbg     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=35:00:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/glamdbg.out # output file name # add %j to id the job
-#SBATCH --error=logs/glamdbg.err  # error file name # add %j to id the job
+#SBATCH --time=00:30:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --output=logs/evglmdbg.out # output file name # add %j to id the job
+#SBATCH --error=logs/evglmdbg.err  # error file name # add %j to id the job
 #   SBATCH -C v100-32g
 
 set -x
