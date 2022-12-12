086aa82183ff1934306d3cf500f519ad00ea27eb
themyrl <loic.themyr@lecnam.net>
2022-12-12 15:32:06 +0100

    rerun nnunet

--- one_runner_jeanzay.sh
+++ one_runner_jeanzay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=m_ctco_gda     # job name
+#SBATCH --job-name=m_usnn_gda     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=02:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/m_ctco_gda%j.out # output file name # add %j to id the job
-#SBATCH --error=logs/m_ctco_gda%j.err  # error file name # add %j to id the job
+#SBATCH --output=logs/m_usnn_gda%j.out # output file name # add %j to id the job
+#SBATCH --error=logs/m_usnn_gda%j.err  # error file name # add %j to id the job
 # # #   SBATCH -C v100-32g
 
 set -x
@@ -38,12 +38,15 @@ module load python/3.8.8
 
 ## multi anno
 # python mainDouble.py -m model=nnunet dataset=us_128_double_jz training=training_128_jz dataset.cv=$1 #m_usnn
+python mainDouble.py -m model=nnunet dataset=us_128_double_jz training=training_128_jz training.name=training_128_jz_gda dataset.cv=$1 #m_usnn_gda
 # python mainDouble.py -m model=cotr dataset=us_128_double_jz training=training_128_jz dataset.cv=$1 #m_usco
 # python mainDouble.py -m model=cotr dataset=us_128_double_jz training=training_128_jz training.name=training_128_jz_gda dataset.cv=$1 #m_usco_gda
 
 # python mainDouble.py -m model=nnunet dataset=ct_128_double_jz training=training_128_jz dataset.cv=$1 #m_ctnn
+# python mainDouble.py -m model=nnunet dataset=ct_128_double_jz training=training_128_jz training.name=training_128_jz_gda dataset.cv=$1 #m_ctnn_gda
+
 # python mainDouble.py -m model=cotr dataset=ct_128_double_jz training=training_128_jz dataset.cv=$1 #m_ctco
-python mainDouble.py -m model=cotr dataset=ct_128_double_jz training=training_128_jz training.name=training_128_jz_gda dataset.cv=$1 #m_ctco_gda
+# python mainDouble.py -m model=cotr dataset=ct_128_double_jz training=training_128_jz training.name=training_128_jz_gda dataset.cv=$1 #m_ctco_gda
 
 
 ### eval
