bc379159739911429ae6ef5236d275f487143bd2
themyrl <loic.themyr@lecnam.net>
2022-12-12 15:47:09 +0100

    rerun nnunet

--- jeanzay.sh
+++ jeanzay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=glam_gdadbg     # job name
+#SBATCH --job-name=unetr_gdadbg     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=logs/glam_gdadbg.out # output file name # add %j to id the job
-#SBATCH --error=logs/glam_gdadbg.err  # error file name # add %j to id the job
+#SBATCH --output=logs/unetr_gdadbg.out # output file name # add %j to id the job
+#SBATCH --error=logs/unetr_gdadbg.err  # error file name # add %j to id the job
 #   SBATCH -C v100-32g
 
 set -x
@@ -71,9 +71,8 @@ module load python/3.8.8
 
 
 ###### Now we try without normalized intensity nor cropforeground
-python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 #glam_gdadbg
-# python mainDouble.py -m model=unetr dataset=ct_128_double_jz training=training_128_jz training.loss=Dice dataset.cv=cv1 #unetr_gdadbg
-# python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 training.only_val=True #glamdbgv2
+# python mainDouble.py -m model=glam dataset=ct_128_double_jz training=training_128_jz dataset.cv=cv1 #glam_gdadbg
+python mainDouble.py -m model=unetr dataset=ct_128_double_jz training=training_128_jz training.loss=Dice dataset.cv=cv1 #unetr_gdadbg
 
 
 
