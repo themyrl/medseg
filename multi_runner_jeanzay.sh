0e506eb321b68ff401746ef19ca76487a7e49de0
themyrl <loic.themyr@lecnam.net>
2022-12-19 11:41:37 +0100

    glam all

--- multi_runner_jeanzay.sh
+++ multi_runner_jeanzay.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv1)
+JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv2)
 
-for i in $(seq 2 5);
+for i in $(seq 3 5);
 do
-  	# JOBID=$(sbatch --parsable --dependency=afterany:${JOBID} one_runner_jeanzay.sh cv${i})
-  	sbatch --parsable  one_runner_jeanzay.sh cv${i}
+  	JOBID=$(sbatch --parsable --dependency=afterany:${JOBID} one_runner_jeanzay.sh cv${i})
+  	# sbatch --parsable  one_runner_jeanzay.sh cv${i}
 done
