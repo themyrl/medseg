8ff4358cfb160130053d3c633f89eb9538bdd2d7
themyrl <loic.themyr@lecnam.net>
2022-12-20 16:20:19 +0100

    nnunnet simple v2

--- TrainerV2.py
+++ TrainerV2.py
@@ -203,13 +203,13 @@ class Trainer():
 
 
         trainData = CustomDataset(self.train_split, transform=train_transforms, iterations=self.iterations,
-                                  crop_size=self.crop_size, log=log, net_num_pool_op_kernel_sizes=self.net_num_pool_op_kernel_sizes, , multi_anno=False)
+                                  crop_size=self.crop_size, log=log, net_num_pool_op_kernel_sizes=self.net_num_pool_op_kernel_sizes, multi_anno=False)
         testData = CustomDataset(self.val_split, transform=test_transforms,
-                                 iterations=0, crop_size=self.crop_size, log=log, type_='test', , multi_anno=False)
+                                 iterations=0, crop_size=self.crop_size, log=log, type_='test', multi_anno=False)
         
         if self.online_validation:
             valData = CustomDataset(self.val_split, transform=val_transforms,
-                                    iterations=0, crop_size=self.crop_size, log=log, type_='val', , multi_anno=False)
+                                    iterations=0, crop_size=self.crop_size, log=log, type_='val', multi_anno=False)
 
         self.train_loader = DataLoader(trainData, batch_size=self.batch_size, shuffle=True,
                                        num_workers=self.num_workers, pin_memory=torch.cuda.is_available())
