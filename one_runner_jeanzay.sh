92361e995a0721db6f072aa13398d5aebb1a4a22
themyrl <loic.themyr@lecnam.net>
2022-12-22 16:58:59 +0100

    eval

--- one_runner_jeanzay.sh
+++ one_runner_jeanzay.sh
@@ -6,8 +6,8 @@
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
-#SBATCH --qos=qos_gpu-t4
+#SBATCH --time=02:00:00             # maximum execution time (HH:MM:SS)
+#SBATCH --qos=qos_gpu-t3
 #SBATCH --output=multilogs/evsv2usco%j.out # output file name # add %j to id the job
 #SBATCH --error=multilogs/evsv2usco%j.err  # error file name # add %j to id the job
 # # #   SBATCH -C v100-32g
