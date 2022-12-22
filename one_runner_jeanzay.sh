9d858be8766891cdcc8e06401b3e2d8c92df4827
themyrl <loic.themyr@lecnam.net>
2022-12-22 11:13:10 +0100

    training glam v2

--- one_runner_jeanzay.sh
+++ one_runner_jeanzay.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=evsv2usco     # job name
+#SBATCH --job-name=v2ctglv2     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=10           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=04:00:00             # maximum execution time (HH:MM:SS)
+#SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --output=logs/evsv2usco%j.out # output file name # add %j to id the job
-#SBATCH --error=logs/evsv2usco%j.err  # error file name # add %j to id the job
+#SBATCH --output=logs/v2ctglv2%j.out # output file name # add %j to id the job
+#SBATCH --error=logs/v2ctglv2%j.err  # error file name # add %j to id the job
 # # #   SBATCH -C v100-32g
 
 set -x
@@ -62,6 +62,11 @@ module load python/3.8.8
 # python mainDoubleV2.py -m model=glam dataset=ct_128_double_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #v2ctgl
 # python mainDoubleV2.py -m model=glam dataset=us_128_double_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #v2usgl
 
+
+python mainDoubleV2.py -m model=glam model.name=GLAMv2 dataset=ct_128_double_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #v2ctglv2
+# python mainDoubleV2.py -m model=glam model.name=GLAMv2 dataset=us_128_double_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #v2usglv2
+
+
 ## simple anno v2
 # python mainV2.py -m model=nnunet dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2ctnn
 # python mainV2.py -m model=nnunet dataset=us_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2usnn
@@ -83,7 +88,7 @@ module load python/3.8.8
 # python mainDouble.py -m model=nnunet dataset=ct_128_double_jz training=training_128_jz dataset.cv=$1 training.only_val=True #m_ctnn
 # python mainDouble.py -m model=cotr dataset=ct_128_double_jz training=training_128_jz dataset.cv=$1 training.only_val=True #m_ctco
 
-python mainV2.py -m model=cotr dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 training.checkpoint.load=True training.only_val=True #evsv2usco
+# python mainV2.py -m model=cotr dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 training.checkpoint.load=True training.only_val=True #evsv2usco
 
 
 
