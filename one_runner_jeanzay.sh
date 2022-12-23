cbb92867e543cb720c08a9ffd838f4cf4782d4ea
themyrl <loic.themyr@lecnam.net>
2022-12-23 11:21:51 +0100

    simple glam

--- one_runner_jeanzay.sh
+++ one_runner_jeanzay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=sv2ctgl     # job name
+#SBATCH --job-name=sv2ctglv2     # job name
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -8,8 +8,8 @@
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
 #SBATCH --time=48:00:00             # maximum execution time (HH:MM:SS)
 #SBATCH --qos=qos_gpu-t4
-#SBATCH --output=multilogs/sv2ctgl%j.out # output file name # add %j to id the job
-#SBATCH --error=multilogs/sv2ctgl%j.err  # error file name # add %j to id the job
+#SBATCH --output=multilogs/sv2ctglv2%j.out # output file name # add %j to id the job
+#SBATCH --error=multilogs/sv2ctglv2%j.err  # error file name # add %j to id the job
 # # #   SBATCH -C v100-32g
 
 set -x
@@ -74,8 +74,8 @@ module load python/3.8.8
 # python mainV2.py -m model=cotr dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2ctco
 # python mainV2.py -m model=cotr dataset=us_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2usco
 
-python mainV2.py -m model=glam dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2ctgl
-# python mainV2.py -m model=glam dataset=us_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2usgl
+python mainV2.py -m model=glam model.name=GLAMv2 dataset=ct_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2ctglv2
+# python mainV2.py -m model=glam model.name=GLAMv2 dataset=us_128_simple_jz_v2 training=training_128_jz_v2 dataset.cv=$1 #sv2usglv2
 
 
 
