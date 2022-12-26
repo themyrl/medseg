984c47975ce110adafde2696b9f3e633b188f527
themyrl <loic.themyr@lecnam.net>
2022-12-26 11:51:06 +0100

    simple eval

--- multi_runner_jeanzay.sh
+++ multi_runner_jeanzay.sh
@@ -2,9 +2,13 @@
 
 
 JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv1)
+JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv2)
+JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv3)
+# JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv4)
+JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv5)
 
-for i in $(seq 2 5);
-do
-  	# JOBID=$(sbatch --parsable --dependency=afterany:${JOBID} one_runner_jeanzay.sh cv${i})
-  	sbatch --parsable  one_runner_jeanzay.sh cv${i}
-done
+# for i in $(seq 2 5);
+# do
+#   	# JOBID=$(sbatch --parsable --dependency=afterany:${JOBID} one_runner_jeanzay.sh cv${i})
+#   	sbatch --parsable  one_runner_jeanzay.sh cv${i}
+# done
