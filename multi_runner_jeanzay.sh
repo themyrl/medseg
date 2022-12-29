b61231e68036fdf0a4127b56231d1e8d1179572a
themyrl <loic.themyr@lecnam.net>
2022-12-29 10:42:03 +0100

    simple eval

--- multi_runner_jeanzay.sh
+++ multi_runner_jeanzay.sh
@@ -2,7 +2,7 @@
 
 
 JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv1)
-JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv2)
+# JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv2)
 JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv3)
 # JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv4)
 JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv5)
