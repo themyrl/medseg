e292b4b1172f7b3f37abfd14f78364e326c91982
themyrl <loic.themyr@lecnam.net>
2023-01-12 17:23:36 +0100

    evaluator

--- multi_runner_jeanzay.sh
+++ multi_runner_jeanzay.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv1)
+# JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv1)
 # JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv2)
 JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv3)
 # JOBID=$(sbatch --parsable  one_runner_jeanzay.sh cv4)
