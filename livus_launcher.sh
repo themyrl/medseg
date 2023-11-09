4f2ea45a9709adcec296bf8108fa154f7943bbbe
themyrl <loic.themyr@lecnam.net>
2023-11-09 23:36:30 +0100

    inference time mesure

--- livus_launcher.sh
+++ livus_launcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=data     # job name
+#SBATCH -A arf@v100
 #SBATCH --ntasks=1                  # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
